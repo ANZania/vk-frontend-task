b3d2a0e746ad8ae687ad4aee6baab62537e2de2b
Anastasiia <zianastasia@yandex.ru>
2021-06-19 11:01:23 +0300

    Добавлены данные для рендера эмоджи
    Реализовано:
    -- Подготовлена верстка для рендера эмоджи из json-файла
    -- Добавлены данные эмоджи
    -- Исправлен баг со слушателями событий - теперь они корректно изменяются при ресайзе окна, вызванном с помощью devtools

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,39 +1,57 @@
-// const addListeners = import('./addListeners');
-const addListeners = () => {
+
+const toggleListeners = (actionType) => {
     const emojiPicker = document.querySelector('.emoji-picker-wrapper');
     let expireTimerId;
 
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent) || (window.clientWidth <= 1024)) {
-        document.addEventListener('click', (event) => {
-            if (event.target.closest('.button-container')) {
-                if (emojiPicker.classList.contains('active')) {
+    const toggleEmojiPickerClick = (event) => {
+        if (!event.target.closest('.emoji-picker-wrapper')) {
+            if (emojiPicker.classList.contains('active')) {
+                emojiPicker.classList.remove('active')
+            } else if (event.target.closest('.button-container') && !emojiPicker.classList.contains('active')) {
+                emojiPicker.classList.add('active')
+            }
+        }
+    }
+
+    const toggleEmojiPickerMouseover = (event) => {
+        if (event.target.closest('.emoji-button')) {
+            if (!emojiPicker.classList.contains('active')) {
+                emojiPicker.classList.add('active')
+            }
+        }
+        if (event.target.closest('.emoji-picker-wrapper')) {
+            clearTimeout(expireTimerId)
+        }
+    }
+
+    const toggleEmojiPickerMouseout = (event) => {
+        if (event.target.closest('.emoji-button') || event.target.closest('.emoji-picker-wrapper')) {
+            if (emojiPicker.classList.contains('active')) {
+                expireTimerId = setTimeout(() => {
                     emojiPicker.classList.remove('active')
-                } else {
-                    emojiPicker.classList.add('active')
-                }
+                }, 300)
             }
-        })
-        alert('mob')
+        }
+    }
+
+    if (actionType === 'add') {
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent) || (window.screen.width <= 1024)) {
+            document.addEventListener('click', toggleEmojiPickerClick, false)
+        } else {
+            document.addEventListener('mouseover', toggleEmojiPickerMouseover, false)
+            document.addEventListener('mouseout', toggleEmojiPickerMouseout, false)
+        }
     } else {
-        document.addEventListener('mouseover', (event) => {
-            if (event.target.closest('.emoji-button')) {
-                if (!emojiPicker.classList.contains('active')) {
-                    emojiPicker.classList.add('active')
-                }
-            }
-            if (event.target.closest('.emoji-picker-wrapper')) {
-                clearTimeout(expireTimerId)
-            }
-        })
-        document.addEventListener('mouseout', (event) => {
-            if (event.target.closest('.emoji-button') || event.target.closest('.emoji-picker-wrapper')) {
-                if (emojiPicker.classList.contains('active')) {
-                    expireTimerId = setTimeout(() => {
-                        emojiPicker.classList.remove('active')
-                    }, 300)
-                }
-            }
-        })
+        document.removeEventListener('click', toggleEmojiPickerClick)
+        document.removeEventListener('mouseover', toggleEmojiPickerMouseout)
+        document.removeEventListener('mouseout', toggleEmojiPickerMouseover)
     }
 }
-addListeners();
+
+
+toggleListeners('add');
+
+window.addEventListener('resize', () => {
+    toggleListeners('remove');
+    toggleListeners('add');
+});
